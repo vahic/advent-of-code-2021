d70526ed7176e0a5a7612083c6546dd66b048db9
Vahic Laydier <vahic@laydier.fr>
2022-11-16 15:10:14 +0100

    D2P1 - solved

--- day2/part1.ts
+++ day2/part1.ts
@@ -1,5 +1,6 @@
 import { isEnumValue } from "../common/utils.ts";
 import { use_day_2_input } from "../inputs/inputs.ts";
+import { match } from 'ts-pattern'
 
 interface SubmarinePosition {
     depth: number,
@@ -26,6 +27,17 @@ function parse_instruction(text: string):Instruction {
     return { direction, distance: parseInt(distanceString) }
 }
 
+function execute_instruction(from:SubmarinePosition, instruction:Instruction):SubmarinePosition{
+    return match(instruction.direction)
+    .with(Direction.FORWARD, () => ({...from, horizontal: from.horizontal + instruction.distance}))
+    .with(Direction.UP, () => ({...from, depth: from.depth - instruction.distance}))
+    .with(Direction.DOWN, () => ({...from, depth: from.depth + instruction.distance}))
+    .exhaustive()
+}
+
+
 const plannedCourse = use_day_2_input().map(parse_instruction)
 
-console.log(plannedCourse)
+const finalPosition = plannedCourse.reduce(execute_instruction, {depth: 0, horizontal: 0})
+
+console.log(finalPosition.depth * finalPosition.horizontal)
